6b312af4ca69223e6ae07da9c53afe3f1ff109b6
Martin Karugaba <martinkarugaba21@gmail.com>
2024-10-18 14:25:43 +0300

    Update eslint config

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -1,10 +1,19 @@
+"use client";
+
 import { useState } from "react";
+
 import { SignInFlow } from "../types";
 
 const AuthScreen = () => {
-  const [state, setState] = useState<SignInFlow>()
-  
-  return <div>AuthScreen</div>;
+  const [state, setState] = useState<SignInFlow>("signIn");
+
+  return (
+    <div className="flex h-full items-center justify-center bg-[#5C3B58]">
+      <div className="md-w-[120px] md:h-auto">
+        AuthScreen
+      </div>
+    </div>
+  );
 };
 
 export default AuthScreen;
