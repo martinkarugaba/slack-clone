0ad4d3cb5557df1934d5b4d6295b9c2626a36d34
Martin Karugaba <martinkarugaba21@gmail.com>
2024-10-19 19:23:53 +0300

    Add sign in with google

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -26,7 +26,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleProviderSignIn = (value: "github" | "google") => {
+  const onProviderSignIn = (value: "github" | "google") => {
     signIn(value);
   };
 
@@ -64,7 +64,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
         <div className="flex flex-col gap-y-2.5">
           <Button
             disabled={false}
-            onClick={() => {}}
+            onClick={() => onProviderSignIn("google")}
             variant="outline"
             size="lg"
             className="relative w-full"
@@ -74,7 +74,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
           </Button>
           <Button
             disabled={false}
-            onClick={() => handleProviderSignIn("github")}
+            onClick={() => onProviderSignIn("github")}
             variant="outline"
             size="lg"
             className="relative w-full"
